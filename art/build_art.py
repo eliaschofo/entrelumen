ca97f5f636aee0c9be1bfeb7c0923cc66efce482
eliaschofo <269343904+eliaschofo@users.noreply.github.com>
2026-09-24 20:46:55 -0300

    Heart of Heliodor icon (before the blessing) replaces the provisional model

--- art/build_art.py
+++ art/build_art.py
@@ -22,7 +22,7 @@ ITEMS = ['atlas', 'raw_lens', 'survey_notes', 'signal_core', 'calibration_frame'
          'ration_bundle', 'routing_matrix', 'propagation_core', 'power_regulator', 'inventory_sensor', 'handling_core',
          'spectral_lens', 'horizon_chart', 'ecosystem_capsule', 'containment_seal', 'ark_bus', 'renewal_engine',
          'habitation_contract', 'terra_arm', 'light_key', 'light_key_broken', 'heliodor_relic_1',
-         'heliodor_relic_2', 'heliodor_relic_3']
+         'heliodor_relic_2', 'heliodor_relic_3', 'heart_of_heliodor']
 AUGMENTS = ['burning', 'echoing', 'ignore_conditions', 'ignore_light', 'ignore_players', 'initial_health', 'max_delay',
             'max_nearby', 'min_delay', 'no_ai', 'player_range', 'redstone_control', 'silent', 'spawn_count', 'spawn_range', 'youthful']
 ITEMS += ['augment_' + a for a in AUGMENTS]
